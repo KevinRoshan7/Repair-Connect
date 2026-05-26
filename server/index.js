46e965d8f7233f1ffd648096ed4e72a2533da1fd
Kevin Roshan <your_github_email@example.com>
2026-05-26 16:56:39 +0530

    Fixed backend syntax

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
   'http://localhost:5173',
   'https://repair-connect-phi.vercel.app'
-]
+],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
